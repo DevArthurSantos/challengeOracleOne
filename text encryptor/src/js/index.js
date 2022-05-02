117a13a75261104e8aed066dfeba01d5ba6fd561
DevArthurSantos <arthursantos.s@hotmail.com>
2022-05-02 19:25:23 -0300

    add function copyText e remover acentos!

--- text encryptor/src/js/index.js
+++ text encryptor/src/js/index.js
@@ -1,3 +1,4 @@
+const copy = document.getElementById("copy");
 const asideNoMSG = document.getElementById("asideNoMSG");
 const divTextEncripyter = document.getElementById("divTextEncripyter");
 const textoEncripitado = document.getElementById("textoEncripitado");
@@ -11,11 +12,11 @@ function mostrarTexto(show, text) {
   if (show === "cripter") {
     asideNoMSG.style.display = "none";
     divTextEncripyter.style.display = "block";
-    divTextEncripyter.textContent = text;
+    textoEncripitado.textContent = text;
   } else if (show === "decripter") {
     asideNoMSG.style.display = "none";
     divTextEncripyter.style.display = "block";
-    divTextEncripyter.innerText = text;
+    textoEncripitado.innerText = text;
   }
 }
 
@@ -32,7 +33,11 @@ function errorTexto() {
 
 function criptografar() {
   if (!textoCompleto.value) return errorTexto();
-  let cripter = textoCompleto.value.toLowerCase().replace(/a/g, "z01s5z");
+  let cripter = textoCompleto.value
+    .toLowerCase()
+    .replace(/a/g, "z01s5z")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
   let cripter1 = cripter.replace(/e/g, "1y1s6y");
   let cripter2 = cripter1.replace(/i/g, "3x4s5x");
   let cripter3 = cripter2.replace(/o/g, "41ws5w");
@@ -43,12 +48,24 @@ function criptografar() {
 
 function descriptografar() {
   if (!textoCompleto.value) return errorTexto();
-  let decripter = textoCompleto.value.toLowerCase().replace(/z01s5z/g, "a");
+  let decripter = textoCompleto.value
+    .toLowerCase()
+    .replace(/z01s5z/g, "a")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
   let decripter1 = decripter.replace(/1y1s6y/g, "e");
   let decripter2 = decripter1.replace(/3x4s5x/g, "i");
   let decripter3 = decripter2.replace(/41ws5w/g, "o");
   let decripter4 = decripter3.replace(/v51s6v/g, "u");
   mostrarTexto("decripter", decripter4);
+}
+
+function copyText() {
+  navigator.clipboard.writeText(textoEncripitado.value);
+  asideNoMSG.style.display = "block";
+  divTextEncripyter.style.display = "none";
+  error.style.color = "#0A3871";
+  error.innerText = "Texto copiado para a area de trasferencia";
 }
 
 encriptar.addEventListener("click", (e) => {
@@ -59,4 +76,9 @@ encriptar.addEventListener("click", (e) => {
 desencriptar.addEventListener("click", (e) => {
   e.preventDefault;
   descriptografar();
+});
+
+copy.addEventListener("click", (e) => {
+  e.preventDefault;
+  copyText();
 });
